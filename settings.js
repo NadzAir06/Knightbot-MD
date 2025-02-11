c49bbf54dccdbc0bd46945ccdd9db491bc753c63
NadzAir06 <158485873+NadzAir06@users.noreply.github.com>
2025-02-11 14:05:34 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'mcnadzair', // Your name
-  ownerNumber: '255620805966', //Your number
+  botOwner: 'zhou', // Your name
+  ownerNumber: '96894325525', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
